56243b600d661613fbc5df075541f5a8cb9e1c4a
alex <alex@example.com>
2020-11-22 05:54:31 +0300

    first commit

---
+++ src/test/java/ru/alexander/project/builder/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexander.project.builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
